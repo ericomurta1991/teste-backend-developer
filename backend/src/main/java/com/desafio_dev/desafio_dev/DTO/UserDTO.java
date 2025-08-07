f1151b950e40ab2ee0cf9573521fc06678cc9190
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-07 09:05:37 -0300

    User Dto, atributos e metodos adicionados

--- backend/src/main/java/com/desafio_dev/desafio_dev/DTO/UserDTO.java
+++ backend/src/main/java/com/desafio_dev/desafio_dev/DTO/UserDTO.java
@@ -1,5 +1,29 @@
 package com.desafio_dev.desafio_dev.DTO;
 
-public class UserDTO {
+import java.io.Serializable;
 
+import com.desafio_dev.desafio_dev.entities.User;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID =1l;
+	
+	private long id;
+	@NotBlank
+	private String name;
+	@NotBlank
+	private String cpf;
+	
+	public UserDTO (User entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.cpf = entity.getCpf();
+	}
 }
