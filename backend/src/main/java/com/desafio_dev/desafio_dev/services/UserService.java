b1c07969e0f1902e8b5537928abb64f36ba1376b
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-07 09:19:28 -0300

    User pagination, find by id, insert, update, delete

--- backend/src/main/java/com/desafio_dev/desafio_dev/services/UserService.java
+++ backend/src/main/java/com/desafio_dev/desafio_dev/services/UserService.java
@@ -1,5 +1,76 @@
 package com.desafio_dev.desafio_dev.services;
 
-public class UserService {
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desafio_dev.desafio_dev.DTO.UserDTO;
+import com.desafio_dev.desafio_dev.entities.User;
+import com.desafio_dev.desafio_dev.repositories.UserRepository;
+import com.desafio_dev.desafio_dev.services.exceptions.DatabaseException;
+import com.desafio_dev.desafio_dev.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService{
+	
+	
+	@Autowired
+	UserRepository repository;
+	
+	@Transactional
+	public Page<UserDTO> findAllPaged(PageRequest pageRequest){
+		Page<User> list = repository.findAll(pageRequest);
+		return list.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional
+	public UserDTO findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+		return new UserDTO(entity);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity = repository.save(entity);
+		
+		return new UserDTO(entity);
+		
+	}
+	
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		try {
+			User entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			return new UserDTO(entity);			
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not Found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID Not Found " + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+	}
+	
+	
 }
