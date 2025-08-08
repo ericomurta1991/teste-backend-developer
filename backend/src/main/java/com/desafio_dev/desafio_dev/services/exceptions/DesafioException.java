edf80ceab668428b87a996c85df82a5bab10febd
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-08 18:10:14 -0300

    DesafioException created

---
+++ backend/src/main/java/com/desafio_dev/desafio_dev/services/exceptions/DesafioException.java
@@ -0,0 +1,11 @@
+package com.desafio_dev.desafio_dev.services.exceptions;
+
+public class DesafioException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DesafioException(String message) {
+        super(message);
+    }
+    
+
+    }
