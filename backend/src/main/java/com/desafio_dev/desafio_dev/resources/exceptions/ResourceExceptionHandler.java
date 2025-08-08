9bc01ccdd3e01ed667c62f9a26f267d4767178e5
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-08 18:09:01 -0300

    ResourceExceptionHandler updated

--- backend/src/main/java/com/desafio_dev/desafio_dev/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/desafio_dev/desafio_dev/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.desafio_dev.desafio_dev.services.exceptions.DatabaseException;
+import com.desafio_dev.desafio_dev.services.exceptions.DesafioException;
 import com.desafio_dev.desafio_dev.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -44,5 +45,20 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DesafioException.class)
+	public ResponseEntity<StandardError> handleDesafioException(DesafioException e, HttpServletRequest request){
+	    HttpStatus status = HttpStatus.BAD_REQUEST;  // 400 para erros de validação ou regras de negócio
+
+	    StandardError err = new StandardError();
+	    err.setTimestamp(Instant.now());
+	    err.setStatus(status.value());
+	    err.setError("Application error");
+	    err.setMessage(e.getMessage());
+	    err.setPath(request.getRequestURI());
+
+	    return ResponseEntity.status(status).body(err);
+	}
+	
 	
 }
