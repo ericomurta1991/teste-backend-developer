d68def0568baca9154d2f2d5439f2027369a5ae6
ericomurta1991 <erico_murta@yahoo.com.br>
2025-08-09 17:52:38 -0300

    UserRepositor updated

--- backend/src/main/java/com/desafio_dev/desafio_dev/repositories/UserRepository.java
+++ backend/src/main/java/com/desafio_dev/desafio_dev/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.desafio_dev.desafio_dev.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.desafio_dev.desafio_dev.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	boolean existsByCpf(String cpf);
+	Optional<User> findByCpf(String cpf);
 }
